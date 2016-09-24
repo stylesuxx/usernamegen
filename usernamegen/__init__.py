6b36197cc1e178ccb7187e88dd54a217347d9b75
Chris Landa <stylesuxx@gmail.com>
2016-09-24 21:46:01 +0200

    Initial pypi setup configuration

--- usernamegen/__init__.py
+++ usernamegen/__init__.py
@@ -2,3 +2,5 @@ from .MarkovChain import (MarkovChain)
 from .Generator import (Generator)
 from Formatter import (Join, Underscore, Capitalize, CapitalizeExceptFirst,
                        AppendNumber, CapitalizeAppendNumber)
+
+__version__ = '0.1.0'
