9e58f9a6c5c3c6276a58e7eef94335f3599f2c5c
Chris Landa <stylesuxx@gmail.com>
2016-09-25 02:19:03 +0200

    Bumped version numbers

--- usernamegen/__init__.py
+++ usernamegen/__init__.py
@@ -3,4 +3,4 @@ from .Generator import (Generator)
 from Formatter import (Join, Underscore, Capitalize, CapitalizeExceptFirst,
                        AppendNumber, CapitalizeAppendNumber)
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
